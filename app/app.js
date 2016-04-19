964662a825386f27d2150f5ca1b71c8f74daf935
Andrew Heebum Kwak <andrew@norux.me>
2016-04-19 20:37:42 +0900

    [16.04.19] 5장 스터디   - 카테고리 추가 (유니크 필터 등)   - 페이지 추가 (range 필터 등)

--- app/app.js
+++ app/app.js
@@ -1,18 +1,18 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var myapp = angular.module('myApp', []);
+var myapp = angular.module('myApp', ["customFilters"]);
 
 myapp.constant("dataUrl", "http://localhost:63342/angularStudy0412/app/data.json")
     .controller('myCtrl', function($scope, $http, dataUrl){
-  $scope.data = {};
+        $scope.data = {};
 
-  $http.get(dataUrl)
-      .success(function(data){
-        $scope.data.products = data;
-      })
-      .error(function(error){
-        console.dir("error")
-      })
+        $http.get(dataUrl)
+            .success(function(data){
+                $scope.data.products = data;
+            })
+            .error(function(error){
+                console.dir("error")
+            })
 });
 
